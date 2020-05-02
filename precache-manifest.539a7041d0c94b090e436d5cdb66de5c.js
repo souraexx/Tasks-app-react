9e658c0d97b1a6ac0bd3fa42e7d05741d03331ea
Souradeep Dutta <sourae.47@gmail.com>
2020-05-02 21:10:52 +0530

    Updates

---
+++ precache-manifest.539a7041d0c94b090e436d5cdb66de5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c8146d90e1f2d9b011d4ca313f7fbf",
+    "url": "/Tasks-app-react/index.html"
+  },
+  {
+    "revision": "3f82d3a84c71454d7521",
+    "url": "/Tasks-app-react/static/css/main.1a19c1ea.chunk.css"
+  },
+  {
+    "revision": "f4f4655483218bc28037",
+    "url": "/Tasks-app-react/static/js/2.7a055a62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tasks-app-react/static/js/2.7a055a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f82d3a84c71454d7521",
+    "url": "/Tasks-app-react/static/js/main.185c1305.chunk.js"
+  },
+  {
+    "revision": "790f9fc039495dc3d448",
+    "url": "/Tasks-app-react/static/js/runtime-main.3b71af89.js"
+  }
+]);
